151cb649e3e31a3e681b261ce1d6eca8c00bf88e
Rodrigo Murillo <rmurillo@creedinteractive.com>
2025-04-20 18:46:27 -0500

    page hero

--- wp-content/themes/miller-wittman-theme/parts/blocks/page-hero-block.php
+++ wp-content/themes/miller-wittman-theme/parts/blocks/page-hero-block.php
@@ -1,6 +1,23 @@
 <?php
 openBlock();
+$image = get_field('image');
+$title = get_field('title');
 ?>
+
+<div class="pageHero scheme--dark">
+    <div class="container">
+
+        <div class="pageHero__image u-of">
+            <?php echo wp_get_attachment_image( $image, 'large') ?>
+        </div>
+        <div class="pageHero__main">
+            <h1 class="h-1">
+                <?php echo $title; ?>
+            </h1>
+        </div>
+
+    </div>
+</div>
 
 <?php
 closeBlock();
