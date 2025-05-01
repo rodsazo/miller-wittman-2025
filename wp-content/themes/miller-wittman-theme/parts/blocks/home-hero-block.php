5bf6cb0b05b6d3b8bd760d159986cd1bef45d013
Rodrigo Murillo <rmurillo@creedinteractive.com>
2025-05-01 18:45:42 -0500

    home textures full size

--- wp-content/themes/miller-wittman-theme/parts/blocks/home-hero-block.php
+++ wp-content/themes/miller-wittman-theme/parts/blocks/home-hero-block.php
@@ -6,7 +6,7 @@ $highlights = get_field('highlights') ?: [];
 foreach ($highlights as $one_highlight) {
     $h_image = $one_highlight['bg_image'];
     $h_text = $one_highlight['text'];
-    list( $i_url, $i_title, $i_text ) = wp_get_attachment_image_src( $h_image, 'large' );
+    list( $i_url, $i_title, $i_text ) = wp_get_attachment_image_src( $h_image, 'full' );
 
     $replacement  = "<span class=\"homeHero__highlight\" style=\"--bg-image: url('{$i_url}')\">{$h_text}</span>";
     $text = str_replace( $h_text, $replacement, $text );
