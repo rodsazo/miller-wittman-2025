63fdc3d74711e21414c42e50d3c852e6a7bb672e
Rodrigo Murillo <rmurillo@creedinteractive.com>
2025-05-22 18:46:26 -0500

    text block

--- wp-content/themes/miller-wittman-theme/parts/blocks/basic-text-block.php
+++ wp-content/themes/miller-wittman-theme/parts/blocks/basic-text-block.php
@@ -2,6 +2,7 @@
 openBlock();
 
 $eyebrow = get_field('eyebrow');
+$heading = get_field('heading');
 $content = get_field('content');
 $link = get_field('link');
 $link = get_link_params( $link );
@@ -9,11 +10,18 @@ $link = get_link_params( $link );
 ?>
 
 <div class="container mt-80 mb-96">
-    <div class="cols cols--4-8">
-        <div>
+    <div class="cols">
+        <div class="flow">
             <h2 class="eyebrow">
                 <?php echo $eyebrow; ?>
             </h2>
+            <?php if( $heading ): ?>
+            <div class="wpContent">
+                <h2>
+                    <?php echo $heading; ?>
+                </h2>
+            </div>
+            <?php endif; ?>
         </div>
 
         <div>
