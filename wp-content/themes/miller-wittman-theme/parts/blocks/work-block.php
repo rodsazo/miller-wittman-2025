3d4ff06accedf9832e2adb6b5eb1baecd31a5932
Rodrigo Murillo <rmurillo@creedinteractive.com>
2025-06-25 19:23:00 -0500

    MW-150 feature (Work Page Block): add manual selection option

--- wp-content/themes/miller-wittman-theme/parts/blocks/work-block.php
+++ wp-content/themes/miller-wittman-theme/parts/blocks/work-block.php
@@ -1,18 +1,30 @@
 <?php
 openBlock();
 
-$items = new WP_Query([
+$items_order = get_field('items_order') ?: 'auto';
+$items_ids   = get_field('items') ?: [];
+
+$query_args = [
     'post_type' => \Theme\CustomPostTypes::WORK,
     'posts_per_page' => -1,
-]);
+];
+
+if( $items_order === 'manual' ) {
+    $query_args['post__in'] = $items_ids;
+    $query_args['orderby'] = 'post__in';
+}
+
+$posts = new WP_Query( $query_args );
 
 ?>
 <div class="workPage scheme--dark">
     <div class="container">
-        <h1 class="h-1 workPage__title"><?php the_field('title'); ?></h1>
+        <h1 class="h-1 workPage__title">
+            <?php the_field('title'); ?>
+        </h1>
 
         <div class="workGrid">
-            <?php while( $items->have_posts()): $items->the_post(); ?>
+            <?php while( $posts->have_posts()): $posts->the_post(); ?>
                 <?php part('work-article'); ?>
             <?php endwhile; wp_reset_postdata(); ?>
         </div>
